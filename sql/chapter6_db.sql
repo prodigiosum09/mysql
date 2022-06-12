5ec865c621ec576605bfc2cc094968a5b0b4b3f0
Yuya Oku <singular0316work@gmail.com>
2022-06-13 01:41:44 +0900

    first commit

---
+++ sql/chapter6_db.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Jun 12, 2022 at 12:53 PM
+-- Server version: 5.7.34
+-- PHP Version: 7.4.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `chapter6_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `table1`
+--
+
+CREATE TABLE `table1` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `name` varchar(40) NOT NULL,
+  `dep` varchar(20) NOT NULL,
+  `role` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `table1`
+--
+
+INSERT INTO `table1` (`id`, `name`, `dep`, `role`) VALUES
+(1, '一郎', 'インフラ部', '部長'),
+(2, '二郎', 'アプリ部', '部長'),
+(3, '三郎', 'コンテナ部', '部長');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `table1`
+--
+ALTER TABLE `table1`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `table1`
+--
+ALTER TABLE `table1`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
